a73a53d11bfc6ab685c492e50d2778b73b070e46
Matt Provost <mprovost@termcap.net>
2019-03-28 13:52:29 +0000

    converts ref to Path and prints last element

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 use std::fs::read_to_string;
 
 #[derive(Debug, Clone)]
-struct ParseError;
+struct GitParseError;
 
 fn main() {
     let git_dir   = Path::new(".git");
@@ -10,13 +10,16 @@ fn main() {
 
     let head_ref = read_to_string(head_path).unwrap();
 
-    print!("{}", resolve_indirect_ref(&head_ref).unwrap());
+    let branch_path = Path::new(resolve_indirect_ref(&head_ref).unwrap());
+
+    print!("{}", branch_path.file_name().unwrap().to_str().unwrap());
+
 }
 
-fn resolve_indirect_ref(refstring: &String) -> Result<&str, ParseError> {
+fn resolve_indirect_ref(refstring: &String) -> Result<&str, GitParseError> {
     if refstring.starts_with("ref: ") {
         Ok(refstring.trim_start_matches("ref: "))
     } else {
-        Err(ParseError)
+        Err(GitParseError)
     }
 }
