8ca9201545cef4acb8c9dd5a37a2265a4e9ff0f8
Matt Provost <mprovost@termcap.net>
2019-03-28 13:37:38 +0000

    adds resolve_indirect_ref() to find ref name

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use std::path::Path;
 use std::fs::read_to_string;
 
+#[derive(Debug, Clone)]
+struct ParseError;
+
 fn main() {
     let git_dir   = Path::new(".git");
     let head_path = git_dir.join("HEAD");
 
     let head_ref = read_to_string(head_path).unwrap();
 
-    print!("{}", head_ref);
+    print!("{}", resolve_indirect_ref(&head_ref).unwrap());
+}
+
+fn resolve_indirect_ref(refstring: &String) -> Result<&str, ParseError> {
+    if refstring.starts_with("ref: ") {
+        Ok(refstring.trim_start_matches("ref: "))
+    } else {
+        Err(ParseError)
+    }
 }
