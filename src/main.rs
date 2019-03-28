48ae75799bd9531cc06e3d6b3769be2bcbd5753f
Matt Provost <mprovost@termcap.net>
2019-03-28 19:39:33 +0000

    resolve_indirect_ref() checks for SHA1 ref

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
-use std::fs::read_to_string;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
 
 #[derive(Debug, Clone)]
 struct GitParseError;
@@ -8,18 +9,30 @@ fn main() {
     let git_dir   = Path::new(".git");
     let head_path = git_dir.join("HEAD");
 
-    let head_ref = read_to_string(head_path).unwrap();
+    let mut head_file = BufReader::new(File::open(head_path).unwrap());
 
-    let branch_path = Path::new(resolve_indirect_ref(&head_ref).unwrap());
+    let mut head_ref = String::new();
+    
+    head_file.read_line(&mut head_ref).unwrap();
 
-    print!("{}", branch_path.file_name().unwrap().to_str().unwrap());
+    let head = &head_ref.trim_end().to_string();
+
+    let branch_path = Path::new(resolve_indirect_ref(head).unwrap());
+
+    println!("{}", branch_path.file_name().unwrap().to_str().unwrap());
 
 }
 
 fn resolve_indirect_ref(refstring: &String) -> Result<&str, GitParseError> {
+    if refstring.ends_with('\n') {
+    }
     if refstring.starts_with("ref: ") {
         Ok(refstring.trim_start_matches("ref: "))
     } else {
-        Err(GitParseError)
+        if refstring.len() == 40 {
+            Ok(refstring)
+        } else {
+            Err(GitParseError)
+        }
     }
 }
